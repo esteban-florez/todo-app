0c4b3089e1c5e28e0dc784a91551536d3ef0b892
esteban-florez <eflorez077@gmail.com>
2024-10-04 14:53:49 -0400

    añadiendo footer

---
+++ src/TodoFooter/TodoFooter.js
@@ -0,0 +1,11 @@
+import './TodoFooter.css';
+
+const TodoFooter = () => {
+  return (
+    <footer className='TodoFooter'>
+      Creado por <a href='https://github.com/esteban-florez'>Esteban Florez</a> 
+    </footer>
+  );
+};
+
+export { TodoFooter };
